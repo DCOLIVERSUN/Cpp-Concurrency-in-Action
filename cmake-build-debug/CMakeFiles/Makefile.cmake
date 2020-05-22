e64846ef42b042862153b47c956675253d028bd3
OliverSun <sunqian18s@ict.ac.cn>
2020-05-22 21:09:41 +0800

    update

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -52,6 +52,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/prac_2_3.dir/DependInfo.cmake"
   "CMakeFiles/prac_2_2.dir/DependInfo.cmake"
   "CMakeFiles/prac_2_1.dir/DependInfo.cmake"
   "CMakeFiles/prac_1_1.dir/DependInfo.cmake"
---
+++ cmake-build-debug/CMakeFiles/prac_2_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prac_2_3.dir/prac_2_3.cpp.o"
+  "prac_2_3"
+  "prac_2_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prac_2_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
