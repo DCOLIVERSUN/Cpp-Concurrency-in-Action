f5a293b6062836000bbcb19227751d0e3b5da087
OliverSun <sunqian18s@ict.ac.cn>
2020-05-24 15:52:40 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/prac_2_8.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prac_2_8.dir/prac_2_8.cpp.o"
+  "prac_2_8"
+  "prac_2_8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prac_2_8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
